28822a73f5cd118bf354788e8406411db056ea6c
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2017-08-24 15:45:58 +0300

    increase log size

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ docker run -dti \
     --name=npm-cache \
     -p 81:81 \
     -v npm-cache-store:/npm-cache-store \
-    --log-opt max-size=1m \
+    --log-opt max-size=100m \
     --log-opt max-file=9 \
     --restart=unless-stopped \
     private/npm-cache
